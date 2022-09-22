cc000655d13a7e74555f4e0f7c315bf562641ffe
Adam Siegel <tobioatom@gmail.com>
2022-09-21 23:01:56 -0700

    adding png

--- cmd/image.go
+++ cmd/image.go
@@ -16,6 +16,7 @@ import (
 var qr bool
 var jpg bool
 var gif bool
+var png bool
 var value string
 
 // imageCmd represents the image command
@@ -41,6 +42,22 @@ to quickly create a Cobra application.`,
 				txt = value
 			}
 			QR.QRFromText(txt)
+		}
+		if png {
+			txt := "./img/kali.png"
+			if len(value) > 0 {
+				txt = value
+			}
+			var scr screen.S
+
+			scr.GetDims()
+			i := img.Img{
+				FilePath: txt,
+				File:     img.PNG,
+				Fit:      img.FitHeight,
+			}
+			i.Dims = util.Coord{X: scr.Width, Y: scr.Height}
+			i.Draw()
 		}
 		if jpg {
 			txt := "./img/image.jpeg"
@@ -81,5 +98,6 @@ func init() {
 	imageCmd.Flags().BoolVarP(&qr, "QR", "q", false, "draws a QR Code that points to https://github.com/x86ed/aura or a value specified by the --value (-v) flag")
 	imageCmd.Flags().BoolVarP(&jpg, "jpg", "j", false, "draws a jpg a value specified by the --value (-v) flag")
 	imageCmd.Flags().BoolVarP(&gif, "gif", "g", false, "draws a gif a value specified by the --value (-v) flag")
+	imageCmd.Flags().BoolVarP(&png, "png", "p", false, "draws a png a value specified by the --value (-v) flag")
 	imageCmd.Flags().StringVarP(&value, "value", "v", "", "string value passed in to method (values with spaces need to be contained in quotes)")
 }
